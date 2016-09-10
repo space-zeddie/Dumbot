7f38c38d557bbe69a22120886d12f2ea5756c854
Alex <alex.frankiv13@gmail.com>
2016-09-10 13:03:07 +0300

    slight fixes, all works now. start working on answers

--- bin/bot.js
+++ bin/bot.js
@@ -2,7 +2,7 @@
 
 var PsychBot = require('../lib/psychbot');
 
-var token = process.env.SLACK_BOT_TOKEN;
+var token = 'xoxb-78023626082-fIjFixMuaL55bR89NXsTaYVg';
 var name = 'psychiatrist';
 
 var psych = new PsychBot({
